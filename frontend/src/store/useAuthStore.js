c0d85f74bcea35789263c536415fbd6726f02d04
ernesto571 <emmanueladesemoye@gmail.com>
2025-09-15 17:11:47 +0100

    project completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5001" 
-  : "/";
+  : "https://fullstack-crypto-app.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
